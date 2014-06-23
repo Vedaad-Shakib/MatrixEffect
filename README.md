d62e6ae5793045c7e7006e5257fd1f66005104fb
Vedaad Shakib <vedaad799@gmail.com>
2014-06-22 19:15:17 -0700

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ MatrixEffect
 ============
 
 This creates the code effect seen in "The Matrix" movie.
+
+For best results, use in terminal with homebrew setting.
+
+<img src="http://images4.alphacoders.com/888/8886.jpg" />
--- matrix.cpp
+++ matrix.cpp
@@ -3,29 +3,32 @@
 #include <chrono>
 #include <thread>
 #include <cstdlib>
+
+using namespace std;
+
 const int width = 158;        // Width of terminal window
 const int flipsPerLine = 5;    // No. of columns changed per line
 const int millisecondsOfSleep = 50; // Delay between lines in millisecond
 int main() {
   srand(time_t(NULL));
   bool switches[width] = { true };
-  const std::string garbage = "1234567890/*-+.,./;[]\\=_~`!@#$%^&*()";
+  const string garbage = "1234567890/*-+.,./;[]\\=_~`!@#$%^&*()";
   const auto glen = garbage.size();
   while (true) {        // Waiting for Ctrl-C
     for (int i = 0; i != width; ++i) {
       if (switches[i]) {
-	std::cout << garbage[rand() % glen];
+	cout << garbage[rand() % glen];
       } else {
-	std::cout << ' ';
+	cout << ' ';
       }
     }
-    std::cout << std::endl;
+    cout << endl;
     for (int i = 0; i != flipsPerLine; ++i) {
       int x = rand() % width;
       switches[x] = !switches[x];
       // Flipping switches
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(millisecondsOfSleep));
+    this_thread::sleep_for(chrono::milliseconds(millisecondsOfSleep));
   }
   return 0;
 }
